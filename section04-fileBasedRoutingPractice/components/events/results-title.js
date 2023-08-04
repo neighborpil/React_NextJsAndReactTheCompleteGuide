3cf678f3b7824d274ec0cb7c0f495f300fb1dc0b
neighborpil <neighborpil@naver.com>
2023-08-04 16:00:05 +0900

    save

---
+++ section04-fileBasedRoutingPractice/components/events/results-title.js
@@ -0,0 +1,22 @@
+import Button from "../ui/button";
+import {useMemo} from "react";
+
+import classes from './results-title.module.css';
+
+function ResultsTitle(props) {
+  const { date } = props;
+  const humanReadableDate = useMemo(() => {
+    return new Date(date).toLocaleDateString('en-US', {
+      month: 'long',
+      year: 'numeric',
+    });
+  }, [date]);
+  return (
+    <section className={classes.title}>
+      <h1>Events in {humanReadableDate}</h1>
+        <Button link={'/events'}>Show all events</Button>
+    </section>
+  );
+}
+
+export default ResultsTitle;
--- section04-fileBasedRoutingPractice/pages/events/index.js
+++ section04-fileBasedRoutingPractice/pages/events/index.js
@@ -1,13 +1,20 @@
 import {getAllEvents} from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
+import {useRouter} from "next/router";
 
 function AllEventsPage() {
+    const router = useRouter();
     const events = getAllEvents();
+
+    function findEventsHandler(year, month) {
+        const fullPath = `/events/${year}/${month}`;
+        router.push(fullPath);
+    }
 
     return (
         <div>
-            <EventsSearch />
+            <EventsSearch onSearch={findEventsHandler}/>
             <EventList items={events}/>
         </div>
     )
