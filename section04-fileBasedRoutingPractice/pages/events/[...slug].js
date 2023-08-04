707190653dc9b1583bd21ba8c1eb6be974ef7d76
neighborpil <neighborpil@naver.com>
2023-08-04 16:36:47 +0900

    save

--- section04-fileBasedRoutingPractice/pages/events/[...slug].js
+++ section04-fileBasedRoutingPractice/pages/events/[...slug].js
@@ -2,13 +2,15 @@ import {useRouter} from "next/router";
 import {getFilteredEvents} from "../../dummy-data";
 import EventList from "../../components/events/event-list";
 import ResultsTitle from "../../components/events/results-title";
+import Button from "../../components/ui/button";
+import ErrorAlert from "../../components/ui/error-alert";
 
 function FilteredEventsPage() {
     const router = useRouter();
 
     const filterData = router.query.slug
 
-    if(!filterData) {
+    if (!filterData) {
         return <p className={'center'}>Loading...</p>
     }
 
@@ -18,20 +20,39 @@ function FilteredEventsPage() {
     const numYear = +filteredYear;
     const numMonth = +filteredMonth;
 
-    if(isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
-        return <p>Invalid filter. Please adjust your values!</p>
+    if (isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 2021 || numMonth < 1 || numMonth > 12) {
+        return (
+            <>
+                <ErrorAlert>
+                    <p>Invalid filter. Please adjust your values!</p>
+                </ErrorAlert>
+                <div className={"center"}>
+                    <Button link={'/events'}>Show all events</Button>
+                </div>
+            </>
+        );
     }
 
     const filteredEvents = getFilteredEvents({year: numYear, month: numMonth});
 
-    if(!filteredEvents || filteredEvents.length === 0) {
-        return <p>No events found for the chosen filter!</p>
+    if (!filteredEvents || filteredEvents.length === 0) {
+        return (
+            <>
+                <ErrorAlert>
+                    <p>No events found for the chosen filter!</p>
+                </ErrorAlert>
+                <div className={"center"}>
+                    <Button link={'/events'}>Show all events</Button>
+                </div>
+            </>
+        );
     }
 
+    const date = new Date(numYear, numMonth - 1);
 
     return (
         <div>
-            <ResultsTitle/>
+            <ResultsTitle date={date}/>
             <EventList items={filteredEvents}/>
         </div>
     );
